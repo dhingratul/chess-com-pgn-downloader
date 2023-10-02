2a6e45140e1d76b22cbe4b78ba18c61a8d6d3c15
dhingratul <dhingra.atul92@gmail.com>
2023-10-02 10:31:36 -0700

    Working version with monthly/ batch downloads

--- scrape_pgn.py
+++ scrape_pgn.py
@@ -5,39 +5,44 @@ import urllib.request
 import argparse
 import logging
 import os
-
+import subprocess
 
 class parseChessComPGN:
-    def __init__(self, username: str):
-        self.username = username
-        self.baseUrl = "https://api.chess.com/pub/player/" + self.username + "/games/"
-        self.archivesUrl = self.baseUrl + "archives"
+    def __init__(self, args) -> None:
+        self.username = args.username
+        self.month = args.month
+        self.year = args.year
+        self.start_year = args.start_year
+        self.start_month = args.start_month
+        # eg: https://api.chess.com/pub/player/hikaru/games/2022/12/pgn
+        self.download_path = args.download_path
 
-    def get_monthly_pgn(self, download_path: str) -> None:
-        if not os.path.exists(download_path):
-            os.makedirs(download_path)
-        f = urllib.request.urlopen(self.archivesUrl)
-        archives = f.read().decode("utf-8")
-        archives = archives.replace('{"archives":["', '","')
-        archivesList = archives.split('","' + self.baseUrl)
-        archivesList[len(archivesList) - 1] = archivesList[
-            len(archivesList) - 1
-        ].rstrip('"]}')
+    def get_monthly_pgn(self, year, month) -> None:
+        baseUrl =  f"https://api.chess.com/pub/player/{self.username}/games/{year}/{month}/pgn"
+        if not os.path.exists(self.download_path):
+            os.makedirs(self.download_path)
+        logging.info("Dowloading for " + year + "_" + month)
+        subprocess.call(["curl", "-o", self.download_path + "/" + year + "_" + month +  ".pgn", baseUrl])
 
-        for i in range(len(archivesList) - 1):
-            url = self.baseUrl + archivesList[i + 1] + "/pgn"
-            filename = archivesList[i + 1].replace("/", "-")
-            urllib.request.urlretrieve(
-                url, f"{download_path}/{self.username}" + filename + ".pgn"
-            )
-            logging.info(filename + ".pgn has been downloaded.")
-        logging.info("All files have been downloaded.")
 
+    def get_all_pgn(self):
+        # All PGNs until the date provided
+        for year in range(int(self.start_year), int(self.year)):
+            for month in range(int(self.start_month), int(self.month)):
+                month = str("%02d" % month)
+                year = str(year)
+                # '%02d' % month
+                self.get_monthly_pgn(year, month)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--username", help="Input Username", type=str)
-    parser.add_argument("--download_path", help="download path", type=str)
+    parser.add_argument("--username", help="Input Username", type=str, default="dhingratul92")
+    parser.add_argument("--download_path", help="download path", type=str, default="data/dhingratul92/pgn")
+    parser.add_argument("--month", help="month", type=str, default="09")
+    parser.add_argument("--year", help="year", type=str, default="2023")
+    parser.add_argument("--start_year", help="start year", type=str, default="2015")
+    parser.add_argument("--start_month", help="start month", type=str, default="03")
     args = parser.parse_args()
-    chess_downloader = parseChessComPGN(args.username)
-    chess_downloader.get_monthly_pgn(args.download_path)
+    chess_downloader = parseChessComPGN(args)
+    # chess_downloader.get_monthly_pgn(args.year, args.month)
+    chess_downloader.get_all_pgn()
